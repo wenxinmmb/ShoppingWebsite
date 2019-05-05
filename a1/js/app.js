71e0fe4932701e2f8c6a25ba595a21e7365186dc
Wenxin Zhou <wenxinmmb@gmail.com>
2019-05-05 00:38:37 -0700

    backup part 1 of a3

--- a1/js/app.js
+++ a1/js/app.js
@@ -3,7 +3,6 @@ function Store(initialStock){
 	// constructor
 	this.stock = initialStock;
 	this.cart = {}; //associate array - an object
-
 };
 
 Store.prototype.addItemToCart = function(itemName){
@@ -25,6 +24,7 @@ Store.prototype.addItemToCart = function(itemName){
 		console.log(itemName +" not found.");
 	}		
 	inactiveTime = 0;
+	//re-render product list
 }
 
 Store.prototype.removeItemFromCart = function(itemName){
@@ -43,6 +43,7 @@ Store.prototype.removeItemFromCart = function(itemName){
 		console.log(itemName +" not found.");
 	}
 	inactiveTime = 0;
+	//re-render product list
 }
 
 function showCart(cart){
@@ -66,9 +67,65 @@ var timerIncrement = function() {
     }
 }
 
+
+function renderProduct(container, storeInstance,itemName){
+	
+	if(store.stock == undefined || store.stock[itemName] == undefined){
+		console.log("Cannot find item in store.");
+		return;
+	}
+	
+	var li1 = document.createElement("li");
+	li1.classList.add("product");
+
+	var div1 = document.createElement("div");
+
+	var name = document.createTextNode(itemName);
+	var image = document.createElement("img");
+	image.setAttribute("src", "images/"+ itemName + "_"+"$"+ store.stock[itemName].price+".png");
+	image.classList.add("itemImage");
+
+	var div2 = document.createElement("div");
+	div2.classList.add("overlay");
+
+	var price = document.createElement("div");
+	price.appendChild(document.createTextNode("$1999"));
+	div2.appendChild(price);
+	
+	console.log(store.stock[itemName])
+	console.log(store.cart[itemName])
+
+	if (store.stock[itemName].quantity > 0){
+		var addBtn = document.createElement("button");
+		addBtn.classList.add("btn-add");
+		addBtn.appendChild(document.createTextNode("Add to Cart"));
+		div2.appendChild(addBtn);
+	}
+
+	if(store.cart[itemName] > 0){
+		var removeBtn = document.createElement("button");
+		removeBtn.classList.add("btn-remove");
+		removeBtn.appendChild(document.createTextNode("Remove from Cart"));
+		div2.appendChild(removeBtn);
+	}
+	div1.appendChild(image);
+	div1.appendChild(name);
+	div1.appendChild(div2);
+	li1.appendChild(div1);
+
+	container.parentNode.replaceChild(li1, container);
+}
+
+
+// renderProductList(container, storeInstance){
+
+// }
+
 window.onload = function() {
 	var addItembtn = document.getElementsByClassName("btn-add");
 	var removeItembtn = document.getElementsByClassName("btn-remove");
+
+	
 
 	// event register
 	for(var i = 0; i < addItembtn.length; i++){
@@ -81,4 +138,10 @@ window.onload = function() {
 	showCartbtn.addEventListener("click", function(){showCart(store.cart)}, false);
 
 	setInterval(timerIncrement, 1000);
+
+	var lists = document.getElementById("productList");
+	var currentContainer = document.createElement("li");
+	lists.appendChild(currentContainer);
+	renderProduct(currentContainer, store, "Box1");
+	
 }
